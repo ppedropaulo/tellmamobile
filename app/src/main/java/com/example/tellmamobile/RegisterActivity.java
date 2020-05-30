7233139df806cf7b3679eaa4c78695ee8f656033
ppedropaulo <pedropaulole@gmail.com>
2020-05-30 14:48:57 -0300

    Login and Register Acitivities

--- app/src/main/java/com/example/tellmamobile/RegisterActivity.java
+++ app/src/main/java/com/example/tellmamobile/RegisterActivity.java
@@ -6,16 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 
-    public void openRegisterActivity(View view) {
-        Intent intent = new Intent(this, RegisterActivity.class);
+    public void returnLoginActivity(View view){
+
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+
     }
 }
