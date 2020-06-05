60dfb45d8bc8d5077496d8ef2cc226b9fd8bca06
mgirardin <matheus.girardin@gmail.com>
2020-06-05 20:03:56 -0300

    :sparkles: Getting user info in ChatListActivity

--- app/src/main/java/com/example/tellmamobile/ChatListActivity.java
+++ app/src/main/java/com/example/tellmamobile/ChatListActivity.java
@@ -13,10 +13,11 @@ public class ChatListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_list);
-
+        String username = UserSession.getInstance(this.getApplicationContext()).getUsername();
+        String user_id = UserSession.getInstance(this.getApplicationContext()).getId();
         ListView listView = (ListView) findViewById(R.id.chat_list_recycler_view);
 
-        String[] chatListDataSet = { "Conversa 1", "Conversa 2", "Conversa 3", "Conversa 4", "Conversa 5", "Conversa 6", "Conversa 7", "Conversa 8", "Conversa 9", "Conversa 10", "Conversa 11", "Conversa 12" };
+        String[] chatListDataSet = {"Conversa 1", "Conversa 2", "Conversa 3", "Conversa 4", "Conversa 5", "Conversa 6", "Conversa 7", "Conversa 8", "Conversa 9", "Conversa 10", "Conversa 11", "Conversa 12" };
 
         ChatListAdapter chatListAdapter = new ChatListAdapter(chatListDataSet, this);
         listView.setAdapter(chatListAdapter);
